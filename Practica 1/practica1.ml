465cf176f647c98b38e75e10a09539172abdb9e5
Daniel López <d.lopez.lopez@udc.es>
2018-11-10 18:00:03 +0100

    Ejercicio traza_af.

--- Practica 1/practica1.ml
+++ Practica 1/practica1.ml
@@ -3,3 +3,27 @@ open Conj;;
 open Auto;;
 open Ergo;;
 open Graf;;
+
+
+(*****************************************************************************
+ *
+ * traza_af : simbolo list -> af -> bool
+ *
+ *****************************************************************************)
+
+let traza_af cadena (Af (_, _, inicial, _, finales) as a) =
+
+   let rec aux = function
+
+        (Conjunto [], _, pasos) ->
+           (false, pasos)
+
+      | (actuales, [], pasos) ->
+           (not (es_vacio (interseccion actuales finales)),pasos)
+
+      | (actuales, simbolo :: t, pasos) ->
+           aux ((epsilon_cierre (avanza simbolo actuales a) a), t, pasos @ [((list_of_conjunto (avanza simbolo actuales a)), (string_of_cadena t))])
+
+   in
+      aux ((epsilon_cierre (Conjunto [inicial]) a), cadena, [([inicial], (string_of_cadena cadena))])
+   ;;
