3bd56348b0655cc9f99129010e5df3c6d474020f
Daniel López <d.lopez.lopez@udc.es>
2018-11-20 21:00:55 +0100

    Revision.

--- Practica 1/practica1.ml
+++ Practica 1/practica1.ml
@@ -11,7 +11,8 @@ let ej25 = af_of_file "../data/ejercicio25.af";;
 let ej26 = af_of_file "../data/ejercicio26.af";;
 let ej26b = af_of_file "../data/ejercicio26b.af";;
 let estados_afn = (Conjunto [(Conjunto [Estado "0"],Estado "0");(Conjunto [Estado "1";Estado "2"],Estado "1");(Conjunto [Estado "2"],Estado "2");(Conjunto [Estado "0";Estado "2"],Estado "3") ; (conjunto_vacio, Estado "4") ]);;
-let cadena = [Terminal "a";Terminal "a";Terminal "a";Terminal "b";Terminal "a";Terminal "c"];;
+let cadena = [Terminal "a";Terminal "a";Terminal "a";Terminal "b";Terminal "a";Terminal "b"];;
+let cadena2 = [Terminal "b";Terminal "b"];;
 
 
 (*****************************************************************************
@@ -72,26 +73,26 @@ let rec siguientes inicial af = function
 (*Devuelve un conjunto de Estados que tienen un estado que es final*)
 let finales estados_afd estados_finales =
   let rec aux = function
-    ([],_) -> []
+    ([],_) -> conjunto_vacio
     | (h::t,estados_finales) ->
       let rec recorrido = function
-        (_,[]) -> []
+        (_,[]) -> conjunto_vacio
         | ((c,e), h::t) -> match pertenece h c with
-                      true -> e :: recorrido ((c,e),t)
+                      true -> Conjunto [e]
                       | false -> recorrido ((c,e),t)
-      in recorrido(h,estados_finales) @ aux(t,estados_finales)
+      in  union (recorrido(h,estados_finales))  (aux(t,estados_finales))
   in aux (list_of_conjunto estados_afd, list_of_conjunto estados_finales)
 ;;
 (*Devuelve los arcos entre el conjunto de estados c*)
 let arcos c (Af (_, alfabeto, _, _, _) as a) =
   let rec aux = function
     ([],_,_,_) -> conjunto_vacio
-    | (h::t,alfabeto,a,l) ->
+    | (h::t,alfabeto,a) ->
       let rec recorrido = function
         (_,[],_,_) -> conjunto_vacio
         | ((c,e),h::t,a,l) -> agregar (Arco_af(e,asociado l (epsilon_cierre(avanza h (epsilon_cierre c a) a) a),h)) (recorrido ((c,e),t,a,l))
-      in union (recorrido (h,alfabeto,a,l)) (aux (t,alfabeto,a,l))
-  in aux(list_of_conjunto c,list_of_conjunto alfabeto,a,list_of_conjunto c)
+      in union (recorrido (h,alfabeto,a,h::t)) (aux (t,alfabeto,a,h::t))
+  in aux(list_of_conjunto c,list_of_conjunto alfabeto,a)
 ;;
 
 let rec estados_afd = function
@@ -102,7 +103,7 @@ let rec estados_afd = function
 let afd_of_afn (Af (_, alfabeto, inicial, _, _) as a) =
 
   let rec aux = function
-  (Af (_, alfabeto, _, _, estados_finales) as a,([],conjunto_tratado),_) -> Af((estados_afd (list_of_conjunto conjunto_tratado)), alfabeto, Estado "0", (arcos conjunto_tratado a), conjunto_of_list (finales conjunto_tratado estados_finales))
+  (Af (_, alfabeto, _, _, estados_finales) as a,([],conjunto_tratado),_) -> Af((estados_afd (list_of_conjunto conjunto_tratado)), alfabeto, Estado "0", (arcos conjunto_tratado a), finales conjunto_tratado estados_finales)
   | (Af (_, alfabeto, _, _, _) as a,(h::f,conjunto_tratado),n) ->
     match existe (function (x,_) -> igual x h) (list_of_conjunto conjunto_tratado) with
       false -> aux(a,(list_of_conjunto (union (conjunto_of_list f) (siguientes h a (list_of_conjunto alfabeto))), agregar (h,Estado (string_of_int n)) conjunto_tratado),n+1)
